1914f49111b03ad3b24706b488cea3a488c28f03
Yerer <yererone0.1@gmail.com>
2020-08-01 14:19:55 -0700

    Updates

---
+++ precache-manifest.798ae282cfb7786bc6142a17d6f32c3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fe2c31a5996688460a7960bfbf69021",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "379f0049d4b24263dbe7",
+    "url": "/login/static/css/main.c36526c9.chunk.css"
+  },
+  {
+    "revision": "a376579b814df1abc13e",
+    "url": "/login/static/js/2.01a10d11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/login/static/js/2.01a10d11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "379f0049d4b24263dbe7",
+    "url": "/login/static/js/main.08aa9649.chunk.js"
+  },
+  {
+    "revision": "6a3821d3272e72f9a0a9",
+    "url": "/login/static/js/runtime-main.f5dd6cad.js"
+  }
+]);
